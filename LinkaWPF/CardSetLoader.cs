cac8ba773621b80d501ff40afaaba3daeffdf958
Pasha Liubetski <pasha.liubetski@yandex.com>
2022-08-20 00:02:11 +0300

    Ещё немного упростил код. Косметические изменения в окне "Описание карточки".

--- LinkaWPF/CardSetLoader.cs
+++ LinkaWPF/CardSetLoader.cs
@@ -8,39 +8,6 @@ namespace LinkaWPF
 {
     public class CardSetLoader
     {
-        public void SaveToFile(string filePath, CardSetFile cardSetFile)
-        {
-            File.Delete(filePath);
-
-            using (var archive = ZipFile.Open(filePath, ZipArchiveMode.Create))
-            {
-
-                List<string> paths = new List<string>();
-                foreach (var card in cardSetFile.Cards)
-                {
-                    string fix = card.ImagePath;
-                    string name = Path.GetFileName(fix);
-                    while (paths.Contains(name))
-                    {
-                        name = "_" + name;
-                    }
-                    paths.Add(name);
-                    card.ImagePath = name;
-                    if (card.ImagePath != null && card.ImagePath != string.Empty) archive.CreateEntryFromFile(fix, name);
-                    if (card.AudioPath != null && card.AudioPath != string.Empty) archive.CreateEntryFromFile(card.AudioPath, Path.GetFileName(card.AudioPath));
-                }
-
-                string json = JsonConvert.SerializeObject(cardSetFile, Formatting.Indented, new CardJsonConverter(typeof(Card)));
-
-                var configEntry = archive.CreateEntry("config.json");
-                using (var writer = new StreamWriter(configEntry.Open()))
-                {
-                    writer.WriteLine(json);
-                }
-
-            }
-        }
-
         public CardSetFile LoadFromFile(string filePath, string tempPath)
         {
             ZipFile.ExtractToDirectory(filePath, tempPath);
--- LinkaWPF/DescriptionWindow.xaml.cs
+++ LinkaWPF/DescriptionWindow.xaml.cs
@@ -14,22 +14,13 @@ namespace LinkaWPF
             Description = description;
             EditField.Text = description;
             EditField.Focusable = editor;
-            WrapButtons.Visibility = editor ? Visibility.Visible : Visibility.Hidden;
         }
 
         public bool Editor { get; }
         public string Description { get; private set; }
 
-        private void Cancel_Click(object sender, RoutedEventArgs e)
+        private void cancelButton_Click(object sender, RoutedEventArgs e)
         {
-            DialogResult = false;
-            Close();
-        }
-
-        private void Save_Click(object sender, RoutedEventArgs e)
-        {
-            DialogResult = true;
-            Description = EditField.Text;
             Close();
         }
     }
